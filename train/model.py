56906b246d0deeb2d651a1961c43d954f51da09f
anacolada <ana.ivan1219@gmail.com>
2019-11-28 16:15:55 +0200

    improved neural net and added deployment details

--- train/model.py
+++ train/model.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class LSTMClassifier(nn.Module):
     """
@@ -12,8 +14,11 @@ class LSTMClassifier(nn.Module):
         super(LSTMClassifier, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-        self.dense = nn.Linear(in_features=hidden_dim, out_features=1)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=0.5)
+        self.gru = nn.GRU(embedding_dim+2*hidden_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=0.5)
+        self.dense1 = nn.Linear(in_features=hidden_dim*2, out_features=hidden_dim)  # *2 because bidirectional=True
+        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        self.dense2 = nn.Linear(in_features=hidden_dim, out_features=1)
         self.sig = nn.Sigmoid()
         
         self.word_dict = None
@@ -22,11 +27,19 @@ class LSTMClassifier(nn.Module):
         """
         Perform a forward pass of our model on some input.
         """
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         x = x.t()
         lengths = x[0,:]
         reviews = x[1:,:]
-        embeds = self.embedding(reviews)
-        lstm_out, _ = self.lstm(embeds)
-        out = self.dense(lstm_out)
-        out = out[lengths - 1, range(len(lengths))]
+        embeds = self.embedding(reviews)  # map the review words to embeddings
+        lstm_out, _ = self.lstm(embeds)  # pass the embeddings through an LSTM module
+        embeds_lstm = torch.cat([embeds, lstm_out], 2)  # concatenate the LSTM output with the embedded input
+        gru, _ = self.gru(embeds_lstm)  # pass the concatenation through a GRU module
+        lstm_out = lstm_out + gru  # summing up the initial LSTM output and the previous GRU output
+        lstm_out, _ = lstm_out.max(dim=0, keepdim=False)  # compression to a 2D array of size batchsize x 2*hidden_dim
+        out = self.dense1(lstm_out)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.dense2(out)
+
         return self.sig(out.squeeze())
--- train/train.py
+++ train/train.py
@@ -68,8 +68,23 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     
     # TODO: Paste the train() method developed in the notebook here.
-
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            model.zero_grad()  # clearing the previous gradients to prevent accumulation
+            outputs = model(batch_X)  # one forward pass
+            loss = loss_fn(outputs, batch_y)  # the overall training loss after forward prop
+            loss.backward()  # the gradients w.r.t. each parameter
+            optimizer.step()  # updating the parameters through backpropagation
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))   # average batch loss
 
 
 if __name__ == '__main__':
@@ -137,12 +152,12 @@ if __name__ == '__main__':
         }
         torch.save(model_info, f)
 
-	# Save the word_dict
+    # Save the word_dict
     word_dict_path = os.path.join(args.model_dir, 'word_dict.pkl')
     with open(word_dict_path, 'wb') as f:
         pickle.dump(model.word_dict, f)
 
-	# Save the model parameters
+    # Save the model parameters
     model_path = os.path.join(args.model_dir, 'model.pth')
     with open(model_path, 'wb') as f:
         torch.save(model.cpu().state_dict(), f)
